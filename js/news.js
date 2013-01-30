21521b2b724c0b95a1b2419fa2655175d0724217
Malek Habibi <malek.habibi@gmail.com>
2013-01-30 13:37:50 +0100

    Update js/news.js

--- js/news.js
+++ js/news.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 $('.news').hover(function (e) {
 	
 	var platform =$('.platform', this).attr('id');
-	$(this).toggleClass(platform);
+	if(!$(this).hasClass('platform'))
+		$(this).toggleClass(platform);
 	$('div:not(".hp, .platform")', this).toggleClass("white");
 	$('.hp, .platform', this).toggleClass(platform+"-active");
 	$('.hp, .platform', this).toggleClass("white-bg");
